8cd3ce763975b34072a5e2ab6f94b414489dece1
moffd234 <moffd234@gmail.com>
2024-03-04 17:26:16 -0500

    Setup getters and setters for new instance variables

--- src/main/java/com/zipcodewilmington/person/Person.java
+++ src/main/java/com/zipcodewilmington/person/Person.java
@@ -10,6 +10,7 @@ public class Person {
     private String job;
     private String favFood;
     private boolean isMarried;
+    private boolean hasTwoArms;
 
     public Person() {
         this.name = "";
@@ -18,6 +19,8 @@ public class Person {
         this.favFood = "";
         this.job = "";
         this.isMarried = false;
+        this.hasTwoArms = true;
+
     }
 
     public Person(int age) {
@@ -33,6 +36,17 @@ public class Person {
         this.age = age;
     }
 
+    public Person(String name, String job, String favFood,
+                  int age, int height, boolean isMarried, boolean hasTwoArms) {
+        this.name = name;
+        this.age = age;
+        this.hasTwoArms = hasTwoArms;
+        this.isMarried = isMarried;
+        this.height = height;
+        this.favFood = favFood;
+        this.job = job;
+    }
+
     public void setName(String name) {
         this.name = name;
     }
@@ -40,11 +54,47 @@ public class Person {
     public void setAge(int age) {
         this.age = age;
     }
-    
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setFavFood(String favFood){
+        this.favFood = favFood;
+    }
+
+    public String getFavFood(){
+        return favFood;
+    }
 
 
     public String getName() {
         return name;
+    }
+
+    public void setJob(String job){
+        this.job = job;
+    }
+    public String getJob(){
+        return job;
+    }
+
+    public void setMarried(boolean isMarried){
+        this.isMarried = isMarried;
+    }
+    public boolean getIsMarried(){
+        return isMarried;
+    }
+
+    public void setHasTwoArms(boolean hasTwoArms){
+        this.hasTwoArms = hasTwoArms;
+    }
+
+    public boolean getHasTwoArms(){
+        return hasTwoArms;
     }
 
     public Integer getAge() {
