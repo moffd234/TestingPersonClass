b920b9c5db97b4e5b52b0ef39c56bf135713425c
moffd234 <moffd234@gmail.com>
2024-03-04 17:17:25 -0500

    completed methods so tests pass

--- src/main/java/com/zipcodewilmington/person/Person.java
+++ src/main/java/com/zipcodewilmington/person/Person.java
@@ -6,30 +6,48 @@ package com.zipcodewilmington.person;
 public class Person {
     private String name;
     private int age;
+    private int height;
+    private String job;
+    private String favFood;
+    private boolean isMarried;
 
     public Person() {
+        this.name = "";
+        this.age = Integer.MAX_VALUE;
+        this.height = Integer.MAX_VALUE;
+        this.favFood = "";
+        this.job = "";
+        this.isMarried = false;
     }
 
     public Person(int age) {
+        this.age = age;
     }
 
     public Person(String name) {
+        this.name = name;
     }
 
     public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
     }
 
     public void setName(String name) {
+        this.name = name;
     }
 
     public void setAge(int age) {
+        this.age = age;
     }
+    
+
 
     public String getName() {
-        return null;
+        return name;
     }
 
     public Integer getAge() {
-        return null;
+        return age;
     }
 }
